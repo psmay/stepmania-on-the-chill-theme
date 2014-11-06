aebc1c1bed1a9f2c56c4d9457ebee3fc8b6410c1
Peter S. May <me@psmay.com>
2014-11-06 14:33:06 -0500

    `ScreenWithMenuElements` header now bluer and whiter.

--- Graphics/ScreenWithMenuElements header/default.lua
+++ Graphics/ScreenWithMenuElements header/default.lua
@@ -17,10 +17,10 @@ t[#t+1] = Def.Quad {
 t[#t+1] = LoadActor("_texture stripe") .. {
 	Name="TextureStripe";
 	InitCommand=cmd(x,-SCREEN_CENTER_X-8;y,-2;horizalign,left;vertalign,top;zoomto,320,50;customtexturerect,0,0,(320/2)/8,50/32);
-	OnCommand=cmd(texcoordvelocity,2,0;skewx,-0.0575;diffuse,color("#594300");diffuserightedge,color("#59430000"));
+	OnCommand=cmd(texcoordvelocity,2,0;skewx,-0.0575;diffuse,color("#000088");diffuserightedge,color("#00008800"));
 };
 t[#t+1] = LoadActor("Header") .. {
-	InitCommand=cmd(y,1;vertalign,top;zoomtowidth,SCREEN_WIDTH+1;diffuse,color("#ffd400"));
+	InitCommand=cmd(y,1;vertalign,top;zoomtowidth,SCREEN_WIDTH+1;diffuse,color("#0000ff"));
 	OnCommand=cmd(croptop,46/60);
 };
 
@@ -28,7 +28,7 @@ t[#t+1] = LoadFont("Common Bold") .. {
 	Name="HeaderShadow";
 	Text=Screen.String("HeaderText");
 	InitCommand=cmd(x,-SCREEN_CENTER_X+26;y,28;zoom,1;horizalign,left;maxwidth,200);
-	OnCommand=cmd(visible,IsVisible();skewx,-0.125;diffuse,BoostColor(color("#ffd40077"),0.375););
+	OnCommand=cmd(visible,IsVisible();skewx,-0.125;diffuse,BoostColor(color("#0000ff77"),0.375););
 	UpdateScreenHeaderMessageCommand=function(self,param)
 		self:settext(param.Header);
 	end;
@@ -37,7 +37,7 @@ t[#t+1] = LoadFont("Common Bold") .. {
 t[#t+1] = Def.Quad {
 	Name="Underline";
 	InitCommand=cmd(x,-SCREEN_CENTER_X+24-4;y,36;horizalign,left);
-	OnCommand=cmd(diffuse,color("#ffd400");shadowlength,2;shadowcolor,BoostColor(color("#ffd40077"),0.25);linear,0.25;zoomtowidth,192;fadeleft,8/192;faderight,0.5;
+	OnCommand=cmd(diffuse,color("#0000ff");shadowlength,2;shadowcolor,BoostColor(color("#0000ff77"),0.25);linear,0.25;zoomtowidth,192;fadeleft,8/192;faderight,0.5;
 		visible,string.len( Screen.String("HeaderText") ) > 0 );
 };
 
@@ -45,7 +45,7 @@ t[#t+1] = LoadFont("Common Bold") .. {
 	Name="HeaderText";
 	Text=Screen.String("HeaderText");
 	InitCommand=cmd(x,-SCREEN_CENTER_X+24;y,26;zoom,1;horizalign,left;shadowlength,0;maxwidth,200);
-	OnCommand=cmd(visible,IsVisible();skewx,-0.125;diffuse,color("#ffd400"););
+	OnCommand=cmd(visible,IsVisible();skewx,-0.125;diffuse,color("#ffffff"););
 	UpdateScreenHeaderMessageCommand=function(self,param)
 		self:settext(param.Header);
 	end;
