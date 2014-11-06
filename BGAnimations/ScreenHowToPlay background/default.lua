3eec8eea5fae2b8234695b3a0bec00cb91cdc204
Peter S. May <me@psmay.com>
2014-11-06 14:15:53 -0500

    Replace `ScreenHowToPlay` and `ScreenWithMenuElements` backgrounds with blue parallax clouds.

--- BGAnimations/ScreenHowToPlay background/default.lua
+++ BGAnimations/ScreenHowToPlay background/default.lua
@@ -1,4 +1,20 @@
-return LoadActor("bg.png") ..{
-	InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;zoomtowidth,SCREEN_WIDTH;zoomtoheight,SCREEN_HEIGHT);
-	OnCommand=cmd(texcoordvelocity,0,-1;customtexturerect,0,0,SCREEN_WIDTH/self:GetWidth(),SCREEN_HEIGHT/self:GetHeight();diffuse,color("0.9,0.9,0.9,1"));
+return Def.ActorFrame {
+	InitCommand=cmd(hide_if,hideFancyElements;);
+	Def.Quad {
+		InitCommand=cmd(scaletoclipped,SCREEN_WIDTH,SCREEN_HEIGHT);
+		OnCommand=cmd(diffuse,color("#000000"));
+	};	
+	-- These actors implement a parallax cloud effect
+	LoadActor("vertical-clouds-1") .. {
+		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;zoomtowidth,SCREEN_WIDTH);
+		OnCommand=cmd(texcoordvelocity,0.0,0.10;diffusecolor,color("#0000ff");diffusealpha,0.5);
+	};
+	LoadActor("vertical-clouds-2") .. {
+		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;zoomtowidth,SCREEN_WIDTH);
+		OnCommand=cmd(texcoordvelocity,0.0,0.02;diffusecolor,color("#4444ff");diffusealpha,0.4);
+	};
+	LoadActor("vertical-clouds-3") .. {
+		InitCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;zoomtowidth,SCREEN_WIDTH);
+		OnCommand=cmd(texcoordvelocity,0.0,0.03;diffusecolor,color("#8888ff");diffusealpha,0.3);
+	};
 };
