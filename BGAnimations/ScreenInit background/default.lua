9b57e2af3c3a52bb437f4d87efb49fe88dc725d4
Peter S. May <me@psmay.com>
2014-11-16 21:47:11 -0500

    Shoved Orbitron into a few places. Nice so far.

--- BGAnimations/ScreenInit background/default.lua
+++ BGAnimations/ScreenInit background/default.lua
@@ -1,4 +1,6 @@
 
+local font_name = "Angular Sans";
+
 local start_delay_increment=0.2;
 local get_start = function (step)
 	return step * start_delay_increment;
@@ -9,7 +11,7 @@ local t = Def.ActorFrame {
 	Def.ActorFrame {
 		InitCommand=cmd(Center);
 
-		LoadFont("Common Bold") .. {
+		LoadFont(font_name) .. {
 			Text="GAME SOFTWARE AND ORIGINAL THEME";
 			InitCommand=cmd(strokecolor,Color("Outline");y,SCREEN_HEIGHT*-0.438;rotationx,90;sleep,get_start(0);smooth,transition_duration;rotationx,0);
 		};
@@ -19,19 +21,19 @@ local t = Def.ActorFrame {
 		LoadActor("ssc-logo") .. {
 			InitCommand=cmd(zoom,0.49;x,SCREEN_WIDTH*0.168;y,SCREEN_HEIGHT*-0.262;rotationx,90;sleep,get_start(2);smooth,transition_duration;rotationx,0);
 		};
-		LoadFont("Common Bold") .. {
+		LoadFont(font_name) .. {
 			Text="http://stepmania.com/";
 			InitCommand=cmd(strokecolor,Color("Outline");zoom,1.2;y,SCREEN_HEIGHT*-0.084;rotationx,90;sleep,get_start(3);smooth,transition_duration;rotationx,0);
 		};
 
-		LoadFont("Common Bold") .. {
+		LoadFont(font_name) .. {
 			Text="NEW THEME DESIGN AND ARTWORK";
 			InitCommand=cmd(strokecolor,Color("Outline");y,SCREEN_HEIGHT*0.045;rotationx,90;sleep,get_start(4);smooth,transition_duration;rotationx,0);
 		};
 		LoadActor("rhythm-hfgk-logo") .. {
 			InitCommand=cmd(zoom,0.587;x,SCREEN_WIDTH*0;y,SCREEN_HEIGHT*0.168;rotationx,90;sleep,get_start(5);smooth,transition_duration;rotationx,0);
 		};
-		LoadFont("Common Bold") .. {
+		LoadFont(font_name) .. {
 			Text="http://rhythm.hfgk.us/";
 			InitCommand=cmd(strokecolor,Color("Outline");zoom,1.2;y,SCREEN_HEIGHT*0.294;rotationx,90;sleep,get_start(6);smooth,transition_duration;rotationx,0);
 		};
