b678275149ad87da9e09527c262abbede99d0619
Peter S. May <me@psmay.com>
2020-07-13 10:39:22 -0400

    WIP Temporarily commented out visual elements of Try Extra Stage on eval screen

--- BGAnimations/ScreenEvaluation decorations/default.lua
+++ BGAnimations/ScreenEvaluation decorations/default.lua
@@ -237,11 +237,12 @@ t[#t+1] = Def.ActorFrame {
 		Condition=THEME:GetMetric( Var "LoadingScreen","Summary" ) == false;
 		OnCommand=cmd(play);
 	};
-	LoadActor( THEME:GetPathG("ScreenStageInformation","Stage extra1" ) ) .. {
-		Condition=THEME:GetMetric( Var "LoadingScreen","Summary" ) == false;
-		InitCommand=cmd(Center);
-		OnCommand=cmd(diffusealpha,0;zoom,0.85;bounceend,1;zoom,1;diffusealpha,1;sleep,0;glow,Color("White");decelerate,1;glow,Color("Invisible");smooth,0.35;zoom,0.25;y,SCREEN_BOTTOM-72);
-	};
+	-- FIXME: Restore something visual here
+	--LoadActor( THEME:GetPathG("ScreenStageInformation","Stage extra1" ) ) .. {
+	--	Condition=THEME:GetMetric( Var "LoadingScreen","Summary" ) == false;
+	--	InitCommand=cmd(Center);
+	--	OnCommand=cmd(diffusealpha,0;zoom,0.85;bounceend,1;zoom,1;diffusealpha,1;sleep,0;glow,Color("White");decelerate,1;glow,Color("Invisible");smooth,0.35;zoom,0.25;y,SCREEN_BOTTOM-72);
+	--};
 };
 t[#t+1] = Def.ActorFrame {
 	Condition=GAMESTATE:HasEarnedExtraStage() and not GAMESTATE:IsExtraStage() and GAMESTATE:IsExtraStage2();
@@ -250,10 +251,11 @@ t[#t+1] = Def.ActorFrame {
 		Condition=THEME:GetMetric( Var "LoadingScreen","Summary" ) == false;
 		OnCommand=cmd(play);
 	};
-	LoadActor( THEME:GetPathG("ScreenStageInformation","Stage extra2" ) ) .. {
-		Condition=THEME:GetMetric( Var "LoadingScreen","Summary" ) == false;
-		InitCommand=cmd(Center);
-		OnCommand=cmd(diffusealpha,0;zoom,0.85;bounceend,1;zoom,1;diffusealpha,1;sleep,0;glow,Color("White");decelerate,1;glow,Color("Invisible");smooth,0.35;zoom,0.25;y,SCREEN_BOTTOM-72);
-	};
+	-- FIXME: Restore something visual here
+	--LoadActor( THEME:GetPathG("ScreenStageInformation","Stage extra2" ) ) .. {
+	--	Condition=THEME:GetMetric( Var "LoadingScreen","Summary" ) == false;
+	--	InitCommand=cmd(Center);
+	--	OnCommand=cmd(diffusealpha,0;zoom,0.85;bounceend,1;zoom,1;diffusealpha,1;sleep,0;glow,Color("White");decelerate,1;glow,Color("Invisible");smooth,0.35;zoom,0.25;y,SCREEN_BOTTOM-72);
+	--};
 };
 return t
